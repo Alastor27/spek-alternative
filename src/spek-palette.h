aef8737bada53d142570bd696f777c37e5961799
Alastor27 <suicidewish@protonmail.com>
2022-03-11 11:13:33 +0100

    Some minor changes

--- src/spek-palette.h
+++ src/spek-palette.h
@@ -7,7 +7,7 @@ enum palette {
     PALETTE_SOX,
     PALETTE_MONO,
     PALETTE_COUNT,
-    PALETTE_DEFAULT = PALETTE_SPECTRUM,
+    PALETTE_DEFAULT = PALETTE_SOX,
 };
 
 uint32_t spek_palette(enum palette palette, double level);
